87b027e45599a65233a70a98e9dc8ddc476d7b7f
vicky <zx@zx.com>
2023-08-10 23:30:20 +0800

    fix

--- src/plugin/awsc226.js
+++ src/plugin/awsc226.js
@@ -107,6 +107,97 @@ function LintIfStatement(path) {
     }
     path.replaceWith(t.ifStatement(test, consequent, alternate))
 }
+function LintLogicalIf(path) {
+    let { operator, left, right } = path.node
+    if (operator !== '&&') {
+        // console.warn(`Unexpected logical operator: ${operator}`)
+        return
+    }
+    if (!t.isExpressionStatement(path.parent)) {
+        // console.warn(`Unexpected parent type: ${path.parent.type}`)
+        return
+    }
+    let node = t.ifStatement(left, t.expressionStatement(right))
+    path.parentPath.replaceWith(node)
+}
+function LintIfTest(path) {
+    let { test, consequent, alternate } = path.node
+    if (!t.isSequenceExpression(test)) {
+        return
+    }
+    if (!t.isBlockStatement(path.parent)) {
+        return
+    }
+    let body = test.expressions
+    let last = body.pop()
+    let before = t.expressionStatement(t.sequenceExpression(body))
+    path.insertBefore(before)
+    path.replaceWith(t.ifStatement(last, consequent, alternate))
+}
+
+function LintSwitchCase(path) {
+    let { test, consequent } = path.node
+    if (consequent.length == 1 && t.isBlockStatement(consequent[0])) {
+        return
+    }
+    let block = t.blockStatement(consequent)
+    path.replaceWith(t.switchCase(test, [block]))
+}
+
+function LintReturn(path) {
+    let { argument } = path.node
+    if (!t.isSequenceExpression(argument)) {
+        return
+    }
+    if (!t.isBlockStatement(path.parent)) {
+        return
+    }
+    let body = argument.expressions
+    let last = body.pop()
+    let before = t.expressionStatement(t.sequenceExpression(body))
+    path.insertBefore(before)
+    path.replaceWith(t.returnStatement(last))
+}
+
+function LintSequence(path) {
+    let body = []
+    for (const item of path.node.expressions) {
+        body.push(t.expressionStatement(item))
+    }
+    let node = t.blockStatement(body, [])
+    let replace_path = path
+    if (t.isExpressionStatement(path.parent)) {
+        replace_path = path.parentPath
+    } else if (!t.isBlockStatement(path.parent)) {
+        console.warn(`Unexpected parent type: ${path.parent.type}`)
+        return
+    }
+    replace_path.replaceWith(node)
+    return
+}
+
+function LintBlock(path) {
+    let { body } = path.node
+    if (!body.length) {
+        return
+    }
+    let changed = false
+    let arr = []
+    for (const item of body) {
+        if (!t.isBlockStatement(item)) {
+            arr.push(item)
+            continue
+        }
+        changed = true
+        for (const sub of item.body) {
+            arr.push(sub)
+        }
+    }
+    if (!changed) {
+        return
+    }
+    path.replaceWith(t.blockStatement(arr))
+}
 
 export default function (code) {
     let ast = parse(code)
@@ -119,8 +210,28 @@ export default function (code) {
     })
     LintConditionalIf(ast)
     traverse(ast, {
+        LogicalExpression: { exit: LintLogicalIf },
+    })
+    traverse(ast, {
         IfStatement: { exit: LintIfStatement },
     })
+    traverse(ast, {
+        IfStatement: { enter: LintIfTest },
+    })
+
+    traverse(ast, {
+        SwitchCase: { enter: LintSwitchCase },
+    })
+    traverse(ast, {
+        ReturnStatement: { enter: LintReturn },
+    })
+    traverse(ast, {
+        SequenceExpression: { exit: LintSequence },
+    })
+    traverse(ast, {
+        BlockStatement: { exit: LintBlock },
+    })
+    //@todo 抽取匿名函数的内容
 
     code = generator(ast, {
         comments: false,
